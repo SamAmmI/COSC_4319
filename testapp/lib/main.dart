bbe0540b3e9555ae27272e331c68562756afbbbb
Johnny <jonsan1210@yahoo.com>
2023-10-19 11:23:26 -0500

    Obtained lib_fromDanielBranch created implemented in loacl project to work on my own machine/branch

---
+++ testapp/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:testapp/auth/auth.dart';
+import 'package:testapp/firebase_options.dart';
+import 'package:testapp/themes/themes.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Key? widgetkey;
+
+  const MyApp({super.key, this.widgetkey});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: darkTheme,
+      home: const AuthPage(),
+    );
+  }
+}
+
