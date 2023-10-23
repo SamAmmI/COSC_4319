0f13e6c233dd5e9d2c46b0ac5955dfc71937b424
Johnny_PC <jonsan1210@yahoo.com>
2023-10-23 18:23:05 -0500

    NutritionScreen v0.1, few visual aspects

--- pantree/lib/screens/nutrition_screen.dart
+++ pantree/lib/screens/nutrition_screen.dart
@@ -16,7 +16,52 @@ class _nutrition_screenState extends State<nutrition_screen> {
         title: Text("Nutrition",
         style: TextStyle(color: Theme.of(context).colorScheme.primary)
         ),
+        centerTitle: true,
       ),
+
+      //BELOW IS THE BODY OF THE SCREEN
+      body: Padding(
+        padding:const EdgeInsets.all(16),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Hello, ", //"Hello, ${userName ?? 'User'}", TO GET THE USERNAME LATER
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Divider(),
+            SizedBox(height: 20),
+            Text("Daily Overview",
+              style:TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(height: 10),
+            
+            Card(
+              elevation: 2,
+              child: Padding(
+                padding: (const EdgeInsets.all(16)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Calories Consumed: consumed/goal"), // You can replace placeholder with actual data.
+                    Text("Proteins: consumed/goal"),
+                    Text("Carbs: consumed/goal"),
+                    Text("Fats: consumed/goal"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      
       drawer: MyDrawer(
         onSignOutTap: (){},
         onFoodInventoryTap: (){},
