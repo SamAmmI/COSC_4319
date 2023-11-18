800674435e238850fc526f641401e18ed940356b
Sam <hsamuel121@gmail.com>
2023-11-18 10:42:57 -0600

    Recipe API small change

--- pantree/lib/components/recipe_view.dart
+++ pantree/lib/components/recipe_view.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'dart:async';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/foundation.dart';
--- pantree/lib/screens/recipe_screen.dart
+++ pantree/lib/screens/recipe_screen.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
