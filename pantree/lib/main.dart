7fac3d49832d3efc2a8fdd7ffcf903b9cba793f6
kkwillijr <kkwillijr@gmail.com>
2023-11-19 21:10:08 -0600

    Update main.dart

--- pantree/lib/main.dart
+++ pantree/lib/main.dart
@@ -1,19 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pantree/auth/auth.dart';
 import 'package:pantree/firebase_options.dart';
 import 'package:pantree/components/theme_notifier.dart';
+import 'package:pantree/screens/food_inventory_screen.dart';
+import 'package:pantree/screens/logout_screen.dart';
+import 'package:pantree/screens/nutrition_screen.dart';
+import 'package:pantree/screens/settings_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeNotifier(),
-      child: const MyApp(),
+      child: MyApp(),
     ),
   );
 }
@@ -21,15 +26,17 @@ void main() async{
 class MyApp extends StatelessWidget {
   final Key? widgetkey;
 
-  const MyApp({super.key, this.widgetkey});
+  const MyApp({
+    super.key,
+    this.widgetkey,
+  });
 
   @override
   Widget build(BuildContext context) {
     var themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: themeNotifier.currentTheme,
-      home: const AuthPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: themeNotifier.currentTheme,
+        home: AuthPage());
   }
 }
